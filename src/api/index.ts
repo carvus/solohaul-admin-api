03422a146d4b9e91831aec32ab232fb794ecf3c6
carvus <armen.demirchyan.ait@gmail.com>
2022-08-10 15:29:52 +0400

    fix: changes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,18 +1,19 @@
 import { Router } from 'express';
 import { TRoute } from './../lib/types';
 import { setupRouter } from '../lib';
-
+import authMiddleware from '../middlewares/authMiddleware';
 
 import newsRouter from './news';
 import authRouter from './auth';
-import authMiddleware from '../middlewares/authMiddleware';
+import servicesRouter from './services';
 
 const router: Router = Router();
 
 
 const apiRoutes: TRoute[] = [
     { path: `/auth`, router: authRouter },
-    { path: `/news`, router: newsRouter, middlewares: [authMiddleware] }
+    { path: `/news`, router: newsRouter, middlewares: [authMiddleware] },
+    { path: `/services`, router: servicesRouter, middlewares: [authMiddleware] }
 ];
 
 setupRouter(apiRoutes, router);
